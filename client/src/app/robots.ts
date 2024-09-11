8087033a18387a994ea5b07f463ad5ad6ac77f3c
andreihar <95883512+andreihar@users.noreply.github.com>
2024-09-10 17:42:31 -0700

    Fixed sitemap and robots

--- client/src/app/robots.ts
+++ client/src/app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_WEBSITE_URL || 'https://www.yourwebsite.com';
+  const baseUrl = process.env.NEXT_PUBLIC_WEBSITE_URL || 'http://localhost:3000/';
 
   return {
     rules: {
--- client/src/app/sitemap.ts
+++ client/src/app/sitemap.ts
@@ -2,17 +2,17 @@ import type { MetadataRoute } from 'next';
 import { getAllPostsMeta } from '@/lib/mdx';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = process.env.NEXT_PUBLIC_WEBSITE_URL || 'https://www.yourwebsite.com';
+  const baseUrl = process.env.NEXT_PUBLIC_WEBSITE_URL || 'http://localhost:3000/';
 
   const projects = (await getAllPostsMeta('project')).map((meta) => ({
-    url: `${baseUrl}/project/${meta.id}`,
+    url: `${baseUrl}project/${meta.id}`,
     lastModified: new Date(),
     changeFrequency: 'monthly' as const,
     priority: 0.8,
   }));
 
   const posts = (await getAllPostsMeta('blog')).map((meta) => ({
-    url: `${baseUrl}/blog/${meta.id}`,
+    url: `${baseUrl}blog/${meta.id}`,
     lastModified: meta.published,
     changeFrequency: 'weekly' as const,
     priority: 0.5,
@@ -22,8 +22,26 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: baseUrl,
       lastModified: new Date(),
-      changeFrequency: 'yearly' as const,
+      changeFrequency: 'weekly' as const,
       priority: 1,
+    },
+    {
+      url: `${baseUrl}about`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly' as const,
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}project`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly' as const,
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}blog`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly' as const,
+      priority: 0.5,
     },
     ...projects,
     ...posts,
